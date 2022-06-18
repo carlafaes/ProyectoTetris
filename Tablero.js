ee8b504fe6e5a61a9e7419ae7b3e5812c49e2038
carlafaes <carlafaes18@hotmail.com>
2022-06-18 19:17:24 -0300

    c

--- Tablero.js
+++ Tablero.js
@@ -10,7 +10,22 @@ class Tablero{
         this.alto= this.filas*this.lado_celda;
         this.posicion= createVector(
             MARGEN_TABLERO,
-            MARGEN_TABLERO + this.lado_celda);//posicion inicial del tablero
+            MARGEN_TABLERO + 2*this.lado_celda);//posicion inicial del tablero
+            
+            //memoria es la variable que se encargara de representar los minos almacenados en el tablero
+            this.minosAlmacenados=[];
+            for(let fila=0; fila<this.filas; fila++){
+                this.minosAlmacenados[fila]=[];//crea una fila vacia
+                for(let columna=0; columna<this.columnas; columna++){
+                    this.minosAlmacenados[fila].push("");   //agrega una columna vacia
+                }
+            }
+    }
+
+    set almacenarMino(tetrimino){
+        for(const pmino of tetrimino.mapaTablero){
+            this.minosAlmacenados[pmino.y][pmino.x]=tetrimino.nombre;
+        }
     }
 
     //la coordenada es una transformacion no lineal donde se aplica un escalamiento(multiplicacion) para el ajuste de las medidas y una traslacion (suma) para el ajuste de las posiciones.
@@ -34,9 +49,8 @@ class Tablero{
                 }
                 let c= this.coordenada(columna,fila);
                 rect(c.x,c.y,this.lado_celda);
-            }
-        
-    }
+            }   
+         }
         pop()
     }
 }
--- Tetrimino.js
+++ Tetrimino.js
@@ -1,3 +1,131 @@
+
+        class Tetrimino{
+            constructor(nombre= random(["Z","I","T","O","S","L","J"])){
+                this.nombre=nombre;
+                let base=tetriminosBase[nombre];
+                this.color=base.color;
+                this.mapa=[]
+                for(const pmino of base.mapa){//crea una copia del mapa
+                    this.mapa.push(pmino.copy());
+                }
+                this.posicion= createVector(int(tablero.columnas / 2),0);
+            }
+
+            //movimiento segun teclas de direccion
+            moverDerecha(){
+                this.posicion.x++;
+                if(this.movimientoErroneo){
+                    this.moverIzquierda();
+                }
+            }
+
+            moverIzquierda(){
+                this.posicion.x--;
+                if(this.movimientoErroneo){
+                    this.moverDerecha();
+                }
+            }
+
+            moverAbajo(){
+                this.posicion.y++;
+                if(this.movimientoErroneo){
+                    this.moverArriba();
+                }
+            }
+            moverArriba(){
+                this.posicion.y--;
+            }
+
+
+            girar(){
+                for(const pmino of this.mapa){
+                    pmino.set(pmino.y, -pmino.x)
+                }
+                
+                if(this.movimientoErroneo){
+                    this.desgirar();
+                }
+            }
+
+
+            desgirar(){
+                for(const pmino of this.mapa){
+                    pmino.set(-pmino.y, pmino.x)
+                }
+            }
+
+            get movimientoErroneo(){
+                let salioDelTablero=!this.estaDentroTablero;
+                return salioDelTablero;
+            }
+
+            get estaDentroTablero(){
+                for(const pmino of this.mapaTablero){
+                    if(pmino.x  < 0 ){
+                        //Evita la salida por la izquierda del tetrimino
+                        return false;
+                    }
+                    if(pmino.x >= tablero.columnas){
+                     //Evita la salida por la derecha del tetrimino
+                        return false;
+                    }
+                    if(pmino.y >= tablero.filas){
+                     //Evita la salida por abajo del tetrimino
+                        return false;
+                    }
+                    return true
+                }
+            }
+
+
+            get mapaTablero(){
+                let retorno=[];//crea una matriz vacia
+                for(const pmino of this.mapa){
+                    let copy=pmino.copy().add(this.posicion);//copia el punto y lo suma con la posicion
+                    retorno.push(copy);//convierte las coordenadas del mapa a coordenadas del tablero
+                }
+                return retorno;
+            }
+
+            get mapaCanvas(){
+                let retorno=[];//crea una matriz vacia
+                for(const pmino of this.mapa){
+                    let copy=pmino.copy().add(this.posicion);//copia el punto y lo suma con la posicion
+                    retorno.push(tablero.coordenada(copy.x,copy.y));//convierte las coordenadas del mapa a coordenadas del tablero
+                }
+                return retorno;
+            }
+
+            //Esta funcion se encargara del procesamiento logico del dibujado del objeto
+            dibujar(){
+                push();
+                fill(this.color);
+                for(const pmino of this.mapaTablero){
+                    rect(pmino.x,pmino.y,tablero.lado_celda);//dibuja un rectangulo en las coordenadas del tablero
+                    push()
+                    noStroke();
+                    fill(255,255,255,100);
+                    beginShape();//inicia una figura
+                    vertex(pmino.x,pmino.y);//dibuja un punto en las coordenadas del tablero
+                    vertex(pmino.x+tablero.lado_celda,pmino.y);//dibuja un punto en las coordenadas del tablero
+                    vertex(pmino.x+tablero.lado_celda,pmino.y+tablero.lado_celda);
+                    endShape(CLOSE);//cierra la figura
+
+                    beginShape();
+                    fill(0,0,0,80);
+                    vertex(pmino.x,pmino.y);
+                    vertex(pmino.x, pmino.y + tablero.lado_celda);
+                    vertex(pmino.x+tablero.lado_celda,pmino.y+tablero.lado_celda);
+                    endShape(CLOSE);
+
+                    rect(pmino.x,pmino.y,tablero.lado_celda,tablero.lado_celda/2);
+                    pop()
+                    // let coordDelTablero=tablero.coordenada(pmino.x,pmino.y);//obtiene las coordenadas del tablero
+                }
+                pop();
+            }
+        }
+
 function crearMapeoBaseTetrimino(){
     tetriminosBase={
         "Z":{
